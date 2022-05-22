14ce61e6768f1b5b8cebbe22600d1a144c25d161
jhillsters32 <100387887+jhillsters32@users.noreply.github.com>
2022-05-21 21:04:36 -0700

    Add files via upload

--- Basic Javascript projects/main.js
+++ Basic Javascript projects/main.js
@@ -1,54 +1,11 @@
-
-function myFunction() { // Defining and naming function being used 
-  var a=4; // Defining the a variable
-  var b=4; // Defining the b variable
-var x= a+b; // Expression type being used 
-document.getElementById("math1").innerHTML=x; //Computing the expression with document.getElementById
+function my_Dictionary() { //Defining Function
+	var Animal= { //Defining the Key pairs
+	Species: "Dog",//Defining the Key pairs
+	Color:"Black",//Defining the Key pairs
+	Breed:"Labrador",//Defining the Key pairs
+	Age:5,//Defining the Key pairs
+	Sound:"Bark!"//Defining the Key pairs
+	};
+	delete Animal.Sound;//Deleting value
+	document.getElementById("Dictionary").innerHTML= Animal.Sound;//Computing the expression with document.getElementById
 }
-
-function myFunction1() { // Defining and naming function being used 
-  var a=6; // Defining the a variable
-  var b=4; // Defining the b variable
-var x= a-b; // Expression type being used
-document.getElementById("math2").innerHTML=x; //Computing the expression with document.getElementById
-}
-
-function myFunction2() { // Defining and naming function being used
-  var a=2; // Defining the a variable
-  var b=2; // Defining the b variable
-var x= a*b; //Expression type being used
-document.getElementById("math3").innerHTML=x; //Computing the expression with document.getElementById
-}
-
-function myFunction3() { // Defining and naming function being used
-  var a=5; // Defining the a variable
-  var b=2; // Defining the b variable
-var x= a%b;// Expression type being used
-document.getElementById("math4").innerHTML=x; //Computing the expression with document.getElementById
-}
-
-
-function myFunction4() { // Defining and naming function being used
-var a = 6; // Defining the a variable
-a++; // Expression type being used
-var b=5 // Defining the b variable
-document.getElementById("math5").innerHTML = b; //Computing the expression with document.getElementById
-}
-
-
-function myFunction5() { // Defining and naming function being used
-  var x = 6; // Defining the x variable
-  x--; // Expression type being used
-  var z=8 // Defining the z variable
-  document.getElementById("math6").innerHTML = z; //Computing the expression with document.getElementById
-  }
-
-
-  Math.random()*(b-a)+a; { // Defining and naming function being used
-  var a=4; // Defining the a variable
-  var b=5; // Defining the b variable
-  document.getElementById("math7").innerHTML=b; //Computing the expression with document.getElementById
-  
-}
-
- 
