e7595b37a10462f9fb77061f285bf95c8567ea0f
jhillsters32 <100387887+jhillsters32@users.noreply.github.com>
2022-05-26 16:25:28 -0700

    Add files via upload

---
+++ Basic Javascript projects/Basic Javascript projects/Project5_comparisons_type_coercion/main.js
@@ -0,0 +1,9 @@
+document.write(typeof "red"+"<br>"); // Defining and naming function being used
+document.write("red"+5+"<br>"); // Defining and naming function being used
+document.write(10==10); // Defining and naming function being used
+document.write(10===10); // Defining and naming function being used
+document.write(5>2&& 10>4); // Defining and naming function being used
+document.write(2<4||4<5); // Defining and naming function being used
+function equal_Function(){ // Defining and naming function being used
+	document.getElementById("equal").innerHTML=!(20>10); //Computing the expression with document.getElementById
+}
