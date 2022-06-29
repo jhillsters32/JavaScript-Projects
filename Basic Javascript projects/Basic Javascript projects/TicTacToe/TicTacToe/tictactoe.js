432c54fdaa12df3f837017677e2e56dcac751817
jhillsters32 <100387887+jhillsters32@users.noreply.github.com>
2022-06-29 15:58:21 -0700

    Add files via upload

--- Basic Javascript projects/Basic Javascript projects/TicTacToe/TicTacToe/tictactoe.js
+++ Basic Javascript projects/Basic Javascript projects/TicTacToe/TicTacToe/tictactoe.js
@@ -70,7 +70,7 @@ function placeXOrO(squareNumber) {
             //drawWinLine function is called to draw line if condition is met.
             function checkWinCondtions() {
                 // X 0,1,2 condition.
-                if (arrayIncludes('OX','1X','2X')) { drawWinLine (50,100,558,100)}
+                if (arrayIncludes('0X','1X','2X')) { drawWinLine (50,100,558,100)}
                 // X 3,4,5 condition.
                 else if (arrayIncludes('3X','4X','5X')) { drawWinLine (50,304,558,304)}
                 // X 6,7,8 condition.
@@ -86,21 +86,21 @@ function placeXOrO(squareNumber) {
                 // X 0,4,8 condition.
                 else if (arrayIncludes('0X','4X','8X')) { drawWinLine (100,100,520,520)}
                 // 0 0,1,2condition.
-                else if (arrayIncludes('00','10','20')) { drawWinLine (50,100,558,100)}
+                else if (arrayIncludes('0O','1O','2O')) { drawWinLine (50,100,558,100)}
                 // 0 3,4,5 condition.
-                else if (arrayIncludes('30','40','50')) { drawWinLine (50,304,558,304)}
+                else if (arrayIncludes('3O','4O','5O')) { drawWinLine (50,304,558,304)}
                 // 0 6,7,8 condition.
-                else if (arrayIncludes('60','70','80')) { drawWinLine (50,508,558,508)}
+                else if (arrayIncludes('6O','7O','8O')) { drawWinLine (50,508,558,508)}
                 // 0 0,3,6 condition.
-                else if (arrayIncludes('00','30','60')) { drawWinLine (100,50,100,558)}
+                else if (arrayIncludes('0O','3O','6O')) { drawWinLine (100,50,100,558)}
                 // 0 1,4,7 condition.
-                else if (arrayIncludes('10','40','70')) { drawWinLine (304,50,304,558)}
+                else if (arrayIncludes('1O','4O','7O')) { drawWinLine (304,50,304,558)}
                 // 0 2,5,8 condition.
-                else if (arrayIncludes('20','50','80')) { drawWinLine (508,50,508,558)}
+                else if (arrayIncludes('2O','5O','8O')) { drawWinLine (508,50,508,558)}
                 // 0 6,4,2 condition.
-                else if (arrayIncludes('60','40','20')) { drawWinLine (100,508,510,90)}
+                else if (arrayIncludes('6O','4O','2O')) { drawWinLine (100,508,510,90)}
                 // 0 0,4,8 condition.
-                else if (arrayIncludes('00','40','80')) { drawWinLine (100,100,520,520)}
+                else if (arrayIncludes('0O','4O','8O')) { drawWinLine (100,100,520,520)}
                 // This condition checks for tie. If none of the above conditions register and 9
                 // squares are selected the code excutes.
                 else if (selectedSquares.length>=9) {
@@ -210,8 +210,8 @@ function placeXOrO(squareNumber) {
         //This line waits 1 second. Then, clear canvas, resets game, an allows clicking again.
         setTimeout(function () { clear (); resetGame (); }, 1000);
 // This function resets the game in the event of a tie or a win.
-function resetGame() {
-}
+{
+function resetGame() }
 //This for loop iterates through each HTML square element.
     for(let i=0; i<9; i++) {
         //This variable gets the HTML element i.
