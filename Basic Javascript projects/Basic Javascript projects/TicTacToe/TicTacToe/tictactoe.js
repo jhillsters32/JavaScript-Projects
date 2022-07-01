37964730d4d0a232c71b0cb12750dacb6c8d5e44
jhillsters32 <100387887+jhillsters32@users.noreply.github.com>
2022-06-30 19:09:48 -0700

    Add files via upload

--- Basic Javascript projects/Basic Javascript projects/TicTacToe/TicTacToe/tictactoe.js
+++ Basic Javascript projects/Basic Javascript projects/TicTacToe/TicTacToe/tictactoe.js
@@ -210,8 +210,8 @@ function placeXOrO(squareNumber) {
         //This line waits 1 second. Then, clear canvas, resets game, an allows clicking again.
         setTimeout(function () { clear (); resetGame (); }, 1000);
 // This function resets the game in the event of a tie or a win.
-{
-function resetGame() }
+    }
+function resetGame() {
 //This for loop iterates through each HTML square element.
     for(let i=0; i<9; i++) {
         //This variable gets the HTML element i.
