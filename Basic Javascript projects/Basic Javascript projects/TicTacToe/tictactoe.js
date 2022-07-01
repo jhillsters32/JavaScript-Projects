91ca3865a3ed8a2cceb14029bda5a3fa8689b82d
jhillsters32 <100387887+jhillsters32@users.noreply.github.com>
2022-06-30 18:23:24 -0700

    Add files via upload

--- Basic Javascript projects/Basic Javascript projects/TicTacToe/tictactoe.js
+++ Basic Javascript projects/Basic Javascript projects/TicTacToe/tictactoe.js
@@ -53,7 +53,7 @@ function placeXOrO(squareNumber) {
         //This variable stores a random number 0-8.
         let pickASquare;
         //This condition allows our while loop to keep trying if a square is selected already.
-        while(!sucess) {
+        while(!success) {
             //A random number between 0 and 8 is selected.
             pickASquare= String(Math.floor(Math.random() * 9));
             //If the random number evaluated returns true, the square hasnt been selected yet.
@@ -63,14 +63,14 @@ function placeXOrO(squareNumber) {
                 //This changes our boolean and ends the loop.
                 success= true;
             };
-
+        }
     }
 }
             //This function parese the selectedSquares array to search for win conditions.
             //drawWinLine function is called to draw line if condition is met.
             function checkWinCondtions() {
                 // X 0,1,2 condition.
-                if (arrayIncludes('OX','1X','2X')) { drawWinLine (50,100,558,100)}
+                if (arrayIncludes('0X','1X','2X')) { drawWinLine (50,100,558,100)}
                 // X 3,4,5 condition.
                 else if (arrayIncludes('3X','4X','5X')) { drawWinLine (50,304,558,304)}
                 // X 6,7,8 condition.
@@ -86,21 +86,21 @@ function placeXOrO(squareNumber) {
                 // X 0,4,8 condition.
                 else if (arrayIncludes('0X','4X','8X')) { drawWinLine (100,100,520,520)}
                 // 0 0,1,2condition.
-                else if (arrayIncludes('00','10','20')) { drawWinLine (50,100,558,100)}
+                else if (arrayIncludes('0O','1O','2O')) { drawWinLine (50,100,558,100)}
                 // 0 3,4,5 condition.
-                else if (arrayIncludes('30','40','50')) { drawWinLine (50,304,558,304)}
+                else if (arrayIncludes('3O','4O','5O')) { drawWinLine (50,304,558,304)}
                 // 0 6,7,8 condition.
-                else if (arrayIncludes('60','70','80')) { drawWinLine (50,508,558,508)}
+                else if (arrayIncludes('6O','7O','8O')) { drawWinLine (50,508,558,508)}
                 // 0 0,3,6 condition.
-                else if (arrayIncludes('00','30','60')) { drawWinLine (100,50,100,558)}
+                else if (arrayIncludes('0O','3O','6O')) { drawWinLine (100,50,100,558)}
                 // 0 1,4,7 condition.
-                else if (arrayIncludes('10','40','70')) { drawWinLine (304,50,304,558)}
+                else if (arrayIncludes('1O','4O','7O')) { drawWinLine (304,50,304,558)}
                 // 0 2,5,8 condition.
-                else if (arrayIncludes('20','50','80')) { drawWinLine (508,50,508,558)}
+                else if (arrayIncludes('2O','5O','8O')) { drawWinLine (508,50,508,558)}
                 // 0 6,4,2 condition.
-                else if (arrayIncludes('60','40','20')) { drawWinLine (100,508,510,90)}
+                else if (arrayIncludes('6O','4O','2O')) { drawWinLine (100,508,510,90)}
                 // 0 0,4,8 condition.
-                else if (arrayIncludes('00','40','80')) { drawWinLine (100,100,520,520)}
+                else if (arrayIncludes('0O','4O','8O')) { drawWinLine (100,100,520,520)}
                 // This condition checks for tie. If none of the above conditions register and 9
                 // squares are selected the code excutes.
                 else if (selectedSquares.length>=9) {
@@ -120,7 +120,7 @@ function placeXOrO(squareNumber) {
                     //returned and our else if condition excutes the drawWinLine function.
                     if (a=== true && b=== true && c=== true) {return true}
                 }
-
+            }
                 //This function makes our body element temporarily unclickable.
                 function disableClick() {
                     //This makes our body unclickable.
@@ -141,7 +141,7 @@ function placeXOrO(squareNumber) {
                         //This line accesses our HTML canvas element.
                         const canvas=document.getElementById('win-lines');
                         //This line gives us access to methods and properties to use on canvas.
-                        const C =canvas.getContext('2d');
+                        const c =canvas.getContext('2d');
                         //This line indicates where the start of a lines x axis is.
                         let x1=coordX1,
                             //This line indicates where the start of a lines y axis is.
@@ -164,7 +164,7 @@ function placeXOrO(squareNumber) {
                         //This method starts a new path
                         c.beginPath();
                         //This method moves us to starting point for our line.
-                        c.moveTo(xl, yl)
+                        c.moveTo(x1, y1)
                         // This method indicates the end point in our line.
                         c.lineTo(x,y)
                         //This method sets the width of line.
@@ -172,7 +172,7 @@ function placeXOrO(squareNumber) {
                         // This sets color of line.
                         x.strokesStyle = 'rgba(70,255,33,.8)';
                         //Draws everything we laid out above.
-                        C.stroke();
+                        c.stroke();
                         //This condition checks if we've reached the endpoints.
                         if (x1<=x2&& y1 <= y2) {
                             //this condition adds 10 to the previous end x endpoint.
@@ -197,7 +197,7 @@ function placeXOrO(squareNumber) {
             //This line starts our animation loop.
             const animationLoop=requestAnimationFrame(clear);
             //This line clears our canvas.
-            C.clearRect(0,0,608,608);
+            c.clearRect(0,0,608,608);
             //This line stops our animation loop.
             cancelAnimationFrame(animationLoop);
         }
@@ -210,8 +210,9 @@ function placeXOrO(squareNumber) {
         //This line waits 1 second. Then, clear canvas, resets game, an allows clicking again.
         setTimeout(function () { clear (); resetGame (); }, 1000);
 // This function resets the game in the event of a tie or a win.
+    }
 function resetGame() {
-    //This for loop iterates through each HTML square element.
+//This for loop iterates through each HTML square element.
     for(let i=0; i<9; i++) {
         //This variable gets the HTML element i.
         let square= document.getElementById(String(i));
